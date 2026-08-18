830e83fa0ed109d1a593d38d4cb04febc3d8fcdd
Rachit Sharma <raunaksharma51905@gmail.com>
2026-08-18 09:50:10 +0530

    Time: 0 ms (100%), Space: 31.3 MB (81.82%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, h = nums.size() - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;  // safer midpoint
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                h = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return -1;  
+    }
+};
